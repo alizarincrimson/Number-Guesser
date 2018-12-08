9e37721d1268e9fdf3db85140dee4be1d279b70b
Hannah Hyde <hannahhyde@gmail.com>
2018-12-08 14:06:15 -0700

    Add challenger name and guess function

--- main.js
+++ main.js
@@ -6,6 +6,32 @@ rangeBtn.addEventListener('click', function(event) {
   event.preventDefault();
   min = document.querySelector("#js-set-min").value;
   max = document.querySelector("#js-set-max").value;
+  function randomNum(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+    console.log(min, max);
+  }
+
   console.log(min, max);
   document.querySelector(".display-range").innerText = `The Current Range is ${min} to ${max}`;
 })
+
+var chalOne = undefined;
+var chalTwo = undefined;
+var guessBtn = document.querySelector("#submit-btn");
+
+guessBtn.addEventListener('click', function(event) {
+  event.preventDefault();
+  chalOne = document.querySelector("#js-name-challenger-one").value;
+  chalTwo = document.querySelector("#js-name-challenger-two").value;
+  guessOne = document.querySelector("#js-guess-challenger-one").value;
+  guessTwo = document.querySelector("#js-guess-challenger-two").value;
+  console.log(chalOne, chalTwo, guessOne, guessTwo);
+})
+
+var resetBtn = document.querySelector("#reset-btn");
+
+// resetBtn.addEventListener('click', function(event) {
+//   event.preventDefault();
+// });
